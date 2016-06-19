a564782e18cc0baf10338bd671b37143c4a28817
Hazert <Hazert@172.16.88.187>
2016-06-19 14:22:28 -0700

    New file

---
+++ src/com/company/Hello.java
@@ -0,0 +1,8 @@
+package com.company;
+
+/**
+ * Created by Hazert on 16/6/19.
+ */
+public class Hello {
+
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Test1");
+        String out = "Hello Git";
+        for (int i = 0; i<10; i=i+2) {
+            System.out.println(out);
+        }
     }
 }
